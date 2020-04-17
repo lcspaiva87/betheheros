7d17e6acc3e7a77bd3c8bf814da74efb1e45dd96
lucas <lcspaiva87@gmail.com>
2020-04-17 18:41:23 -0300

    deploy

---
+++ frontand/build/precache-manifest.70280ed41525a3a15c63a04d8cab70b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e25f7b97be44c745f2b3",
+    "url": "/static/css/main.0fe29b6f.chunk.css"
+  },
+  {
+    "revision": "e25f7b97be44c745f2b3",
+    "url": "/static/js/main.4e31d7eb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ac109ce884aaf0ed341d",
+    "url": "/static/js/2.de621766.chunk.js"
+  },
+  {
+    "revision": "21a6d733818da1702a7c085092d29cbb",
+    "url": "/static/media/logo.21a6d733.svg"
+  },
+  {
+    "revision": "1f4b5508873e87c04ab679e647a7954b",
+    "url": "/static/media/heroes.1f4b5508.png"
+  },
+  {
+    "revision": "f28ad1df365660dcdff968d95c007373",
+    "url": "/index.html"
+  }
+];
